b0df39cbc4d76847ec461f8358d9997ffc09bd04
HungNV <hungambrose123@gmail.com>
2026-02-23 22:05:46 +0700

    initialized project

---
+++ src/main/java/com/hungnv/springrealtimechat/SpringrealtimechatApplication.java
@@ -0,0 +1,13 @@
+package com.hungnv.springrealtimechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrealtimechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrealtimechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hungnv/springrealtimechat/SpringrealtimechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungnv.springrealtimechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrealtimechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
