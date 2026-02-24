0b27dad63a6c58194c0410b10cd2be4c9d306f1b
HungNV <hungambrose123@gmail.com>
2026-02-24 11:07:04 +0700

    setup environemnt, add message related functions

--- src/main/java/com/hungnv/springrealtimechat/model/ChatMessage.java
+++ src/main/java/com/hungnv/springrealtimechat/model/ChatMessage.java
@@ -1,10 +1,14 @@
 package com.hungnv.springrealtimechat.model;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ChatMessage {
     long id;
